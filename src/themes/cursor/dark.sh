6daca49787dd954bf6c08cd91c3c9838d0402918
Gabriel Moreno <gantoreno@gmail.com>
2026-01-17 20:05:38 -0400

    feat: add cursor dark theme

--- src/themes/cursor/dark.sh
+++ src/themes/cursor/dark.sh
@@ -14,11 +14,11 @@ declare -gA THEME_COLORS=(
     # Session Indicator
     [session-bg]="#a8cc7c"        # Normal session bg (teal)
     [session-fg]="#141414"        # Session text
-    [session-prefix-bg]="#efb080" # Prefix mode (yellow)
+    [session-prefix-bg]="#f1b467" # Prefix mode (yellow)
     [session-copy-bg]="#505050"   # Copy mode (cyan)
 
     # Windows
-    [window-active-base]="#aaa0fa"   # Active window (teal)
+    [window-active-base]="#87a6c4"   # Active window (teal)
     [window-inactive-base]="#505050" # Inactive windows
 
     # Pane Borders
@@ -28,9 +28,9 @@ declare -gA THEME_COLORS=(
     # Health States
     [ok-base]="#505050"           # Normal state (neutral)
     [good-base]="#a8cc7c"         # Success (green)
-    [info-base]="#82d2ce"         # Info (teal)
-    [warning-base]="#efb080"      # Warning (yellow)
-    [error-base]="#82d2ce"        # Error (red)
+    [info-base]="#87a6c4"         # Info (teal)
+    [warning-base]="#f1b467"      # Warning (yellow)
+    [error-base]="#87a6c4"        # Error (red)
     [disabled-base]="#6e6e6e"     # Disabled (gray)
 
     # Messages
